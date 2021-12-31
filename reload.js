9e93a76e8d87c9e7749916cb3eae95318d78b442
tz2008 <54493503+tz2008@users.noreply.github.com>
2021-12-31 09:34:02 +0800

    Delete reload.js

--- reload.js
+++
@@ -1,9 +0,0 @@
-$httpAPI("POST", "/v1/profiles/reload", {}, data => {
-    $notification.post("配置重载","成功","")
-    $done({
-        title: "Profile Reload",
-        content: "配置档案重载成功",
-        icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
-        "icon-color": "#8B81C3",
-     })
-    });
